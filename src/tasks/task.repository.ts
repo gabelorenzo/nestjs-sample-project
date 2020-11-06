d382d388abcfeb36534bf1934b7962812b9f1c9c
gabelorenzo <gtlorenzo93@gmail.com>
2020-11-05 23:33:13 -0500

    Define OneToMany relationship between User and Task entities and perform authorization for creating a Task for a user

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,3 +1,4 @@
+import { User } from 'src/auth/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -6,7 +7,6 @@ import { Task } from './task.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-
   /**
    * Gets an array of Tasks from the database, optionally filtering down the results.
    * @param filterDto The GetTasksFilterDto object used to filter down the Tasks.
@@ -20,7 +20,10 @@ export class TaskRepository extends Repository<Task> {
     }
 
     if (search) {
-      query.andWhere('LOWER(task.title) LIKE :search OR task.description LIKE :search', { search: `%${search.toLowerCase()}%` });
+      query.andWhere(
+        'LOWER(task.title) LIKE :search OR task.description LIKE :search',
+        { search: `%${search.toLowerCase()}%` },
+      );
     }
 
     const tasks = await query.getMany();
@@ -31,14 +34,16 @@ export class TaskRepository extends Repository<Task> {
    * Gets a single Task by ID from the database.
    * @param id The unique identifier of the Task to retrieve.
    */
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
 
     const task = new Task();
     task.title = title;
     task.description = description;
     task.status = TaskStatus.OPEN;
+    task.user = user;
     await task.save();
+    delete task.user;
 
     return task;
   }
