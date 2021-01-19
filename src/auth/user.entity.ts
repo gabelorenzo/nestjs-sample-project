4670cd0f02ccd19e0c132ee210f65ff8da3e93b0
gabelorenzo <gtlorenzo93@gmail.com>
2021-01-18 23:05:57 -0500

    Unit testing for JwtStrategy

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -31,6 +31,11 @@ export class User extends BaseEntity {
   )
   tasks: Task[];
 
+  /**
+   * Validates a plaintext password by hashing it and comparing it with the user's actual password.
+   * @param password The plaintext password that will be hashed.
+   * @return True if the hashed input password matches the password hash from the database.
+   */
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bcrypt.hash(password, this.salt);
     return hash === this.password;
